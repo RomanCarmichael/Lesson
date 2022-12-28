7c3b0e5815d8efe6472c64b6ff21a36a104cb669
RomanCarmichael <120959203+RomanCarmichael@users.noreply.github.com>
2022-12-27 21:55:37 -0800

    Update if-then-else-statements.js

--- if-then-else-statements.js
+++ if-then-else-statements.js
@@ -2,3 +2,9 @@ if (true) {
   console.log('Hello from IF_THEN')
 }
 //Hello earthlings
+
+// if n1 is greater than both n2 and n3, n1 is the largest
+
+// if n2 is greater than both n1 and n3, n2 is the largest
+
+// if n3 is greater than both n1 and n2, n3 is the largest
